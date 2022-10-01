48047b3d4fcf89e4caf2ed1ed3d9662e1884405e
Tribhuwan <mathincosmos@gmail.com>
2022-10-02 00:11:38 +0530

    implement find

--- bst.js
+++ bst.js
@@ -1,4 +1,5 @@
-const prettyPrint = (node, prefix = "", isLeft = true) => {
+const prettyPrint = (node = null, prefix = "", isLeft = true) => {
+  if (!node) return;
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
@@ -23,6 +24,7 @@ class BST {
     this.arr = arr;
     this.root = this.#buildTree(this.arr, 0, this.arr.length - 1);
   }
+
   #buildTree(arr, low, end) {
     if (low > end) return null;
     let mid = Math.floor((low + end) / 2);
@@ -45,7 +47,7 @@ class BST {
     return root;
   }
   delete(val, root = this.root) {
-    if (root == null) return ;
+    if (root == null) return;
     if (val > root.value) root.right = this.delete(val, root.right);
     else if (val < root.value) root.left = this.delete(val, root.left);
     else {
@@ -64,11 +66,26 @@ class BST {
     }
     return root;
   }
+  find(val, root = this.root) {
+    if (root == null || root.value == val) return root;
+    if (root.value > val) return this.find(val, root.left);
+    else return this.find(val, root.right);
+  }
 }
 
-let bst = new BST([1, 2, 45, 2, 12, 3]);
+function randomArr() {
+  let arr = [];
+  let n = 7;
+  while (n) {
+    let x = Math.floor(Math.random() * 19) + 1;
+    if (!arr.includes(x)) {
+      arr.push(x);
+      n--;
+    }
+  }
+  return arr;
+}
+
+let bst = new BST([1, 234, 232, 42, 11, 45, 25]);
 prettyPrint(bst.root);
-bst.insert(10);
-prettyPrint(bst.root);
-bst.delete(3);
-prettyPrint(bst.root);
+console.log(bst.find(11));
